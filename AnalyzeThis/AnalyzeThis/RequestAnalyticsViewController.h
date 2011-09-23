449c88ff8e5e5e26be3d226eebf42f1c8dae9e04
Jabari Bell <jbell@mcdpartners.com>
2011-09-23 17:31:17 -0400

    adding mac osx example for analytics retrieval reference

---
+++ AnalyzeThis/AnalyzeThis/RequestAnalyticsViewController.h
@@ -0,0 +1,16 @@
+//
+//  RequestAnalyticsViewController.h
+//  AnalyzeThis
+//
+//  Created by Jabari Bell on 9/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RequestAnalyticsViewController : UIViewController {
+    
+}
+
+@end
